36f1db5dcac7ef8d8f460af9b5d4bb098053f8bc
Alejandro Gongora (Contractor) <x244911@LUV.AD.SWACORP.COM>
2018-10-03 13:51:28 -0500

    Sockets added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
-const  publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnect');
+    });
+});
 
 app.use(express.static(publicPath));
-app.listen(port, () => console.log(`Server up on ${port}`));
+server.listen(port, () => console.log(`Server up on ${port}`));
